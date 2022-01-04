5b14401e1a61cc197fc0af03749e16d06446be89
veso266 <veso266@gmail.com>
2022-01-04 20:48:42 +0100

    Update default.js
    Chenge to process.env.PORT so app wil now where to bind to when on Heroku

--- rest-api/config/default.js
+++ rest-api/config/default.js
@@ -7,7 +7,7 @@ module.exports = {
       version: '0.0.1'
     },
     server: {
-      port: process.env.API_PORT || 8080
+      port: process.env.PORT || 8080
     },
 	db: {
 		uri: process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/api',

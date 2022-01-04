8bff2e64b0f3ae4e1494149978ba63f9e8668975
veso266 <veso266@gmail.com>
2022-01-04 20:42:41 +0100

    Update app.js
    Added Connecition poll incase server closes connection

--- rest-api/app/app.js
+++ rest-api/app/app.js
@@ -58,7 +58,13 @@ server.listen(config.server.port, () => {
         logging: false,
         define: {
             paranoid: true
-        }
+        },
+	  pool: {
+    		max: 5,
+    		min: 0,
+    		acquire: 30000,
+    		idle: 10000
+  	}
   });
 
     //Init models

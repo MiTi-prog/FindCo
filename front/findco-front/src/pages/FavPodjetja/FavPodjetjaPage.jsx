866fef396fd720fcd5f125e567b88655cdba5b3c
Mitja Sepec <mitja.sepec99@gmail.com>
2022-01-04 21:44:45 +0100

    footer dobiva podobo z linki

--- front/findco-front/src/pages/FavPodjetja/FavPodjetjaPage.jsx
+++ front/findco-front/src/pages/FavPodjetja/FavPodjetjaPage.jsx
@@ -1,13 +1,16 @@
 import React, { Fragment } from "react";
 import FavPodjetja from "../../components/FavPodjetja/FavPodjetja";
-
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
 import useScrollToTop from "../../hooks/useScrollToTop";
 
 const PodjetjaFavPage = () => {
     useScrollToTop();
     return (
         <Fragment>
+            <Header/>
             <FavPodjetja page={true} />
+            <Footer/>
         </Fragment>
     );
 };

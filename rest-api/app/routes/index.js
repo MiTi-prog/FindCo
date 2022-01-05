831e70890c4f4594f393ff12a7a0be7f56d79459
veso266 <veso266@gmail.com>
2022-01-05 20:55:44 +0100

    Update index.js
    added /api to all api routes

--- rest-api/app/routes/index.js
+++ rest-api/app/routes/index.js
@@ -6,21 +6,18 @@ module.exports = function routes(server) {
   var APIV1Controller = require('../controllers/apiV1Controller');
 
   //Not-Auth routes
-  server.post('/v1/register', APIV1Controller.register);
-  server.post('/v1/login', APIV1Controller.login);
-  server.get('/v1/contractors', APIV1Controller.GetAlllContractors);
-
-  server.get('/api/hello', APIV1Controller.list);
-  server.get('/api/hello/:id', APIV1Controller.getId);
+  server.post('/api/v1/register', APIV1Controller.register);
+  server.post('/api/v1/login', APIV1Controller.login);
+  server.get('/api/v1/contractors', APIV1Controller.GetAlllContractors);
 
   //Restricted routes
-  server.post('/v1/user/edit', rjwt({ secret: config.JWT_SECRET }), APIV1Controller.EditUser);
-  server.del('/v1/user/delete', rjwt({ secret: config.JWT_SECRET }), APIV1Controller.DeleteUser);
-  server.get('/v1/contractors/:id', rjwt({ secret: config.JWT_SECRET }), APIV1Controller.GetContractor);
+  server.post('/api/v1/user/edit', rjwt({ secret: config.JWT_SECRET }), APIV1Controller.EditUser);
+  server.del('/api/v1/user/delete', rjwt({ secret: config.JWT_SECRET }), APIV1Controller.DeleteUser);
+  server.get('/api/v1/contractors/:id', rjwt({ secret: config.JWT_SECRET }), APIV1Controller.GetContractor);
 
   //Company
-  server.post('/v1/company/add', rjwt({ secret: config.JWT_SECRET }), APIV1Controller.CompanyAdd);
-  server.post('/v1/company/edit', rjwt({ secret: config.JWT_SECRET }), APIV1Controller.CompanyEdit);
-  server.del('/v1/company/delete', rjwt({ secret: config.JWT_SECRET }), APIV1Controller.CompanyDelete);
+  server.post('/api/v1/company/add', rjwt({ secret: config.JWT_SECRET }), APIV1Controller.CompanyAdd);
+  server.post('/api/v1/company/edit', rjwt({ secret: config.JWT_SECRET }), APIV1Controller.CompanyEdit);
+  server.del('/api/v1/company/delete', rjwt({ secret: config.JWT_SECRET }), APIV1Controller.CompanyDelete);
 
 }
